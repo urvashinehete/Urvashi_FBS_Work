39a4ff4c75657f15eba107c1636d6ec32b4b202d
URVASHI NEHETE <urvashinehete83@gmail.com>
2026-03-12 21:51:19 +0530

    Delete q11.py
    delete

--- q11.py
+++
@@ -1,8 +0,0 @@
-amount = int(input("Enter the amount:"))
-notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-print("Minimum number of notes:")
-for note in notes:
-    count = amount // note 
-    if count > 0:
-        print(note, ":", count)
-        amount = amount % note
