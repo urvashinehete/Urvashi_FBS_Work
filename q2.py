e69d97b643c60317a244c38bb9c24128d43a268a
URVASHI NEHETE <urvashinehete83@gmail.com>
2026-03-12 21:52:26 +0530

    Delete q2.py
    delete

--- q2.py
+++
@@ -1,8 +0,0 @@
-#input temprature in celsius
-c = float(input("Enter temerature in celsius:"))
-
-#convert the farenheit
-f = (9* c / 5) + 32
-
-#Display result
-print("Temperature in Farenheit =",f)
